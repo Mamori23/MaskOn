1c683477eaa96ced4554b98ca684be5e941db62f
Tiffany Silverio <tsilverio@nyc.yearup.org>
2020-01-02 15:55:53 -0500

    Created accessrecord and files for pages, now to work on server.

--- server.rb
+++ server.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require "sinatra/activerecord/rake"
+require_relative './server'
+
+set :database, {adapter: 'sqlite3', database: 'maskon.sqlite3'}
+enable :sessions
 
 get '/' do
 end
