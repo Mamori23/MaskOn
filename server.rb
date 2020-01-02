0c08b0a54aa33284e096728ec23d03a8b77da268
Tiffany Silverio <tsilverio@nyc.yearup.org>
2020-01-02 16:31:20 -0500

    mirgatuin

--- server.rb
+++ server.rb
@@ -7,4 +7,21 @@ set :database, {adapter: 'sqlite3', database: 'maskon.sqlite3'}
 enable :sessions
 
 get '/' do
+    erb :home
+end
+
+get '/signup' do
+    erb :signup
+end
+
+get '/login' do
+    erb :login
+end
+
+get '/logout' do
+    erb :logout
+end
+
+get '/profile' do
+    erb :profile
 end
