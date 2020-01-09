1fa207e165f712f89fbc7a686caa4c1b16095fec
Tiffany Silverio <tsilverio@nyc.yearup.org>
2020-01-08 21:57:44 -0500

    post now show on database, now need to show on page

--- server.rb
+++ server.rb
@@ -19,21 +19,20 @@ get '/login' do
 end
 
 post '/login' do
-    user = User.find_by(email: params[:email])
+    @user = User.find_by(email: params[:email])
     given_password = params[:password]
-    if user.password == given_password
-        session[:user_id] = user.id
-        session[:user_name] = [user.first_name].join(' ')
-        puts 'Welcome to the Velvet Room'
-        redirect './profile'
+    if @user.password == given_password
+        session[:user_id] = @user.id
+        session[:user_name] = @user.first_name
+        pp session[:user_id]
+        redirect '/profile'
     else
         puts 'Fool'
-     redirect './login'
+     redirect '/login'
     end
 end
 
 get '/signup' do
-    @user = User.new(params[:user])
     erb :signup
 end
 
@@ -41,24 +40,39 @@ post '/signup' do
     @user = User.new(params[:user])
     if @user.valid?
         @user.save
-        redirect '/login'
+        session[:user_id] = @user.id
+        redirect '/profile'
     else
-        redirect './signup'
+        redirect '/signup'
     end 
 end
 
 get '/profile' do
+    @posts = Post.where(user_id: session[:user_id])
+    puts @posts
+    
+    # session[:user_id] = @user.id
     erb :profile
+end
+
+
+get '/profile/:id' do
+        @searchedID = params[:id]
+        @user = User.find_by(id: params[:id])
+        @posts = Post.where(user_id: params[:id])
+        puts @user.id
+        erb :profile
     end
 
 
 post '/profile' do
-   @post = Post.new(user_id: session[:user_id])
-   post = Post.find_by(params['content'])
-   if @post.valid?
-          @post.save
-    else
+    @post = Post.new(params[:post])
+    if @post.valid?
+        @post.user_id = session[:user_id]
+           @post.save
         redirect '/profile'
+        else
+            redirect '/'
     end
 end
 
@@ -70,17 +84,6 @@ get '/logout' do
 end
 
 
-# get '/dashboard' do
-#     erb :dashboard
-# end 
-
-# post '/dashboard' do
-#     post = Post.new(content: params[:content], email: @user.email, user_id: session[user_id] )
-#     if @post.vaild?
-#         @post.save  
-#        redirect '/dashboard'
-#     end
-# end
 
 get '/cancel' do
     erb :cancel
